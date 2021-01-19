88167dff185d41f4dcaf9a143764e4769689e593
Roberto Junior Amarante Calderón <Kyonru@users.noreply.github.com>
2021-01-19 18:57:29 -0400

    Splash screen (#50)
    * Add splash screen
    
    * Bump version
    
    * Add keystore configuration

--- android/app/src/main/java/com/justareviewapp/MainActivity.java
+++ android/app/src/main/java/com/justareviewapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.justareviewapp;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
